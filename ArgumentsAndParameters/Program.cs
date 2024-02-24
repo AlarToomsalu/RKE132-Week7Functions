5608cbb95ccd12e135d8956fb5b72706b135f69c
Alar Toomsalu <alar.toomsalu@tktk.ee>
2024-02-24 12:16:21 +0200

    add week7 code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+Console.WriteLine("Enter something");
+string userInput = Console.ReadLine();
+
+PrintAnyWord(userInput);
+
+
+static void PrintAnyWord(string anyString) //anySring- Parameter
+{
+    anyString = anyString.ToUpper(); 
+    for (int i = 0;i < 5;i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
+
+
+
+
+
+
+
+
